40b3458b2126f6d5ca0ac004ef272ec741827510
Felipe Ignacio Diaz Alburquenque <felipe.diaz@gmail.com>
2022-04-17 22:57:06 -0400

    :zap: [Add] Minified images

--- src/shared/Animations.js
+++ src/shared/Animations.js
@@ -91,27 +91,6 @@ const titleNavBarVariant = {
   },
 }
 
-const variantsSlide = {
-  enter: (direction) => {
-    return {
-      x: direction > 0 ? 1000 : -1000,
-      opacity: 0,
-    }
-  },
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction) => {
-    return {
-      zIndex: 0,
-      x: direction < 0 ? 1000 : -1000,
-      opacity: 0,
-    }
-  },
-}
-
 const variantsNavSlideButton = {
   open: {
     y: 0,
@@ -131,7 +110,6 @@ const variantsNavSlideButton = {
 }
 
 export {
-  variantsSlide,
   whileHoverCard,
   whileHoverCardExit,
   whileHoverSpriteImage,
