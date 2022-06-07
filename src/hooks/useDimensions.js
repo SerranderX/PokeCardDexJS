aa7fd07acec1d0e3cb879e87dfeee00936bf1ba1
beatriz.lara <beatriz.lara@xintec.cl>
2022-06-06 20:08:48 -0400

    Comit de prueba

--- src/hooks/useDimensions.js
+++ src/hooks/useDimensions.js
@@ -3,6 +3,8 @@ import { useRef, useEffect } from 'react'
 const useDimensions = (ref) => {
   const dimensions = useRef({ width: 0, height: 0 })
 
+  console.log(".")
+
   useEffect(() => {
     dimensions.current.width = ref.current.offsetWidth
     dimensions.current.height = ref.current.offsetHeight
